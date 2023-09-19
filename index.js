29ea84312b87a705a0af76afa6bd116decabd3ee
Yash007Dev <145476308+Yash007Dev@users.noreply.github.com>
2023-09-20 00:19:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-
+// Define constants for API access
 const fetchCharacterApi = "https://gateway.marvel.com/v1/public/characters";
-const ts = 1;
-const publicKey = "0a48b66c5a52e6794100099555521ded";
-// const private1 = "a7367e6ad1ca43201b5984358e5df2823226e4ed";
-const hash = "d6b07ef4cc9c9b4f38214d904294ed4b";
+const ts = 1; // Timestamp (you may want to use a dynamic timestamp)
+const publicKey = "0a48b66c5a52e6794100099555521ded"; // Marvel API public key
+const hash = "d6b07ef4cc9c9b4f38214d904294ed4b"; // Hash for authentication
 
+// Initialize an empty array to store superhero data
 var superheroArrayList = [];
 
+// Get references to HTML elements
 const superheroList = document.getElementById("superhero-list");
 const searchKey = document.getElementById("search-key");
 
-async function fetchAllSuperhero(){
+// Function to fetch all superheroes
+async function fetchAllSuperhero() {
     var resp = await fetch(`${fetchCharacterApi}?ts=${ts}&apikey=${publicKey}&hash=${hash}`);
 
     var data = await resp.json();
@@ -20,10 +22,10 @@ async function fetchAllSuperhero(){
     superheroArrayList = results;
 
     addToList(results);
-
 }
 
-async function fetchSuperheroWithName(name){
+// Function to fetch superheroes with a specific name
+async function fetchSuperheroWithName(name) {
     var resp = await fetch(`${fetchCharacterApi}?ts=${ts}&apikey=${publicKey}&hash=${hash}&nameStartsWith=${name}`);
 
     var data = await resp.json();
@@ -35,32 +37,38 @@ async function fetchSuperheroWithName(name){
     addToList(results);
 }
 
-function addToList(results){
+// Function to add superheroes to the HTML list
+function addToList(results) {
     superheroList.innerHTML = "";
-    const li = document.createElement('li');
-    results.map((item)=>{
+    results.map((item) => {
         const li = document.createElement('li');
         li.innerHTML = `<div class="container">
                             <p> ${item.name} </p>
                             <img height="300" width="300" src=${item.thumbnail.path}.${item.thumbnail.extension} />
                             <a target="_blank" href="details.html?id=${item.id}"> <button> <u> Basic Details </u> </button> </a>
-                        </div>` ;
+                        </div>`;
 
         superheroList.append(li);
+    })
+}
 
-    })}
-
-searchKey.addEventListener('keyup', ()=>{
+// Event listener for keyup in the search input
+searchKey.addEventListener('keyup', () => {
     const searchKeyVal = searchKey.value.trim();
-    
-    if (searchKeyVal ==0){
+
+    // If the search input is empty, fetch all superheroes
+    if (searchKeyVal == 0) {
         fetchAllSuperhero();
     }
 
-    if(searchKeyVal.length < 2){
+    // If the search input length is less than 2 characters, return
+    if (searchKeyVal.length < 2) {
         return;
     }
+
+    // Fetch superheroes with the entered name
     fetchSuperheroWithName(searchKeyVal);
 })
 
+// Fetch all superheroes initially when the page loads
 fetchAllSuperhero();
